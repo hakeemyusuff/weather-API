d3c27396e21f14ce4ee812fa7ce7fa207d588975
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-03-26 21:29:10 +0100

    replaced local redis with upstash redis

--- weather_API/settings.py
+++ weather_API/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import redis
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,7 +132,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": config("location"),
+        "LOCATION": config("LOCATION"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
